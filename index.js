c4289dc4a80c4ba524b4dd04087e0732fbba77b8
Edward Cooper <eddcooper79@gmail.com>
2015-06-20 16:21:40 +0000

    modifying index.js to use contents of index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+    var buffer = fs.readFileSync('index.html')
+    response.send(buffer.toString())
+    
 })
 
 app.listen(app.get('port'), function() {
